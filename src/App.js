7d2440c9660b4acdb43e3da899ca0413ee9f6606
luckylab200903 <kumarkeshavsingh200903@gmail.com>
2023-10-30 22:17:16 +0530

    this is the netflix gpt

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 className="text-3xl text-green-400 font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
